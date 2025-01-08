713b4bfba4ea9a64d7bf4d5a44c563bafbd9f28c
Akaikumogo <roboservise24@gmail.com>
2025-01-08 20:37:57 +0500

    done

--- src/pages/SelectionOfApartments.tsx
+++ src/pages/SelectionOfApartments.tsx
@@ -39,12 +39,12 @@
 
 import { useState } from 'react';
 import background from '../assets/Appartments/back.png';
-import { Button } from 'antd';
+import { Button, ConfigProvider, Drawer } from 'antd';
 import { FaRegLightbulb, FaLightbulb } from "react-icons/fa";
-import { MdOutlineKeyboardDoubleArrowLeft } from 'react-icons/md';
+import { MdMenu, MdOutlineKeyboardDoubleArrowLeft } from 'react-icons/md';
 import { useNavigate } from 'react-router';
 
-const blockCoordinates:{id:number,cord:{top?:string,left?:string,bottom?:string,right?:string,transform?:string},customStyle?:string}[] = [
+const blockCoordinates:{id:number,cord?:{top?:string,left?:string,bottom?:string,right?:string,transform?:string},customStyle?:string}[] = [
    { id: 1, cord: { top: "130px", left: "630px",  transform:"SkewY(-10deg) rotate(-5deg)"}, customStyle: " w-[45px] h-[80px]" },
    { id: 2, cord: { top: "150px", left: "580px",  transform:"SkewY(-10deg) rotate(-5deg)"}, customStyle: " w-[45px] h-[50px]" },
    { id: 3, cord: { top: "160px", left: "540px",  transform:"SkewY(-10deg) rotate(-5deg)"}, customStyle: " w-[35px] h-[80px]" },
@@ -107,19 +107,49 @@ const blockCoordinates:{id:number,cord:{top?:string,left?:string,bottom?:string,
    { id: 60, cord: { top: "325px", left: "890px",  transform:" rotate(-30deg)"}, customStyle: " w-[70px] h-[40px]" },
    { id: 61, cord: { top: "360px", left: "920px",  transform:" rotate(-30deg)"}, customStyle: " w-[70px] h-[50px]" },
    //
-   { id: 62, cord: { top: "415px", left: "950px", transform: "rotate(-45deg)" }, customStyle: "w-[50px] h-[60px]" },
-   { id: 63, cord: { top: "465px", left: "985px", transform: "rotate(-30deg)" }, customStyle: "w-[70px] h-[55px]" },
-   { id: 64, cord: { top: "510px", left: "1035px", transform: "rotate(-30deg)" }, customStyle: "w-[70px] h-[80px]" },
-   { id: 65, cord: { top: "560px", left: "1110px", transform: "rotate(-30deg)" }, customStyle: "w-[70px] h-[80px]" },
-   { id: 66, cord: { top: "610px", left: "1150px", transform: "rotate(-30deg)" }, customStyle: "w-[80px] h-[70px]" },
-   { id: 67, cord: { top: "660px", left: "1200px", transform: "rotate(-30deg)" }, customStyle: "w-[120px] h-[120px]" },
-
-
+     { id: 62, cord: { top: "400px", left: "950px", transform: "rotate(-20deg)" }, customStyle: "w-[50px] h-[52px]" },
+     { id: 63, cord: { top: "435px", left: "995px", transform: "rotate(-30deg)" }, customStyle: "w-[50px] h-[55px]" },
+     { id: 64, cord: { top: "480px", left: "1025px", transform: "rotate(-30deg)" }, customStyle: "w-[80px] h-[70px]" },
+     { id: 65, cord: { top: "530px", left: "1100px", transform: "rotate(-30deg)" }, customStyle: "w-[70px] h-[68px]" },
+     { id: 66, cord: { top: "575px", left: "1160px", transform: "rotate(-30deg)" }, customStyle: "w-[80px] h-[59px]" },
+     { id: 67, cord: { top: "610px", left: "1210px", transform: "rotate(-30deg)" }, customStyle: "w-[90px] h-[104px]" },
+     { id: 68, cord: { top: "700px", left: "1290px", transform: "rotate(-30deg)" }, customStyle: "w-[100px] h-[104px]" },
+   
+     { id: 69, cord: { top: "360px", left: "1030px",transform: "rotate(5deg)"}, customStyle: "w-[60px] h-[90px]" },
+     { id: 70, cord: { top: "395px", left: "1085px",transform: "rotate(8deg)"}, customStyle: "w-[60px] h-[100px]" },
+     { id: 71, cord: { top: "435px", left: "1135px",transform: "rotate(20deg)"}, customStyle: "w-[70px] h-[100px]" },
+     { id: 72, cord: { top: "480px", left: "1190px",transform: "rotate(15deg) "}, customStyle: "w-[70px] h-[100px]" },
+     { id: 73, cord: { top: "510px", left: "1250px",transform: "rotate(15deg) "}, customStyle: "w-[80px] h-[100px]" },
+     { id: 74, cord: { top: "570px", left: "1325px",transform: "rotate(15deg) "}, customStyle: "w-[80px] h-[100px]" },
+     { id: 75, cord: { top: "630px", left: "1400px",transform: "rotate(15deg) "}, customStyle: "w-[80px] h-[110px]" },
+     { id: 76, cord: { top: "680px", left: "1480px",transform: "rotate(15deg) "}, customStyle: "w-[80px] h-[110px]" },
+     { id: 77, cord: { top: "330px", left: "1100px",transform: "rotate(0deg) "}, customStyle: "w-[70px] h-[70px]" },
+     { id: 78, cord: { top: "300px", left: "1165px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[50px] h-[90px]" },
+     { id: 79, cord: { top: "270px", left: "1215px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[50px] h-[90px]" },
+     { id: 80, cord: { top: "380px", left: "1200px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 81, cord: { top: "360px", left: "1255px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 82, cord: { top: "340px", left: "1310px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 83, cord: { top: "440px", left: "1315px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 84, cord: { top: "420px", left: "1365px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 85, cord: { top: "400px", left: "1420px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 86, cord: { top: "510px", left: "1420px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 87, cord: { top: "480px", left: "1470px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 88, cord: { top: "460px", left: "1520px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[55px] h-[90px]" },
+     { id: 89, cord: { top: "590px", left: "1520px",}, customStyle: "w-[65px] h-[90px]" },
+     { id: 90, cord: { top: "560px", left: "1590px",}, customStyle: "w-[65px] h-[90px]" },
+     { id: 91, cord: { top: "300px", left: "50px",}, customStyle: "w-[65px] h-[30px]" },
+     { id: 92, cord: { top: "340px", left: "75px",}, customStyle: "w-[95px] h-[70px]" },
+     { id: 93, cord: { top: "410px", left: "120px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[80px] h-[50px]" },
+     { id: 94, cord: { top: "455px", left: "125px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[80px] h-[50px]" },
+     { id: 95, cord: { top: "500px", left: "145px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[80px] h-[40px]" },
+     { id: 96, cord: { top: "535px", left: "185px",transform: "rotate(-10deg) SkewY(-10deg) "}, customStyle: "w-[80px] h-[110px]" },
+   
    
  ];
 const SelectionOfApartments = () => {
   const [currentApartment, setCurrentApartment] = useState(0);
   const [light, setLight] = useState(false);
+  const [openDrawer,setOpenDrawer]=useState(false)
 
   const getCurrentImage = () => {
     return "";
@@ -144,9 +174,11 @@ const SelectionOfApartments = () => {
           shape="circle"
           size="large"
            className=" select-none absolute top-10 left-10  z-[55]"
-          icon={light ? <FaRegLightbulb /> : <FaLightbulb  className='text-yellow-500'/>}
-          onClick={() => setLight(!light)}
+          // icon={light ? <FaRegLightbulb /> : <FaLightbulb  className='text-yellow-500'/>}
+          icon={<MdMenu className="text-3xl" />}
+          onClick={() => setOpenDrawer(!openDrawer)}
         />
+       
    
 
       <img
@@ -160,9 +192,9 @@ const SelectionOfApartments = () => {
           <div
             key={block.id}
          
-            className={`absolute border border-red-500 text-black p-2 rounded z-50 transform ${block.customStyle}`}
+            className={`absolute bordwer border-rwed-500 text-black p-2 rounded z-50 transform ${block.customStyle}`}
             style={{
-               backgroundColor: `hsla(${block.id * 20}, 70%, 50%, 0.5)`,
+              //  backgroundColor: `hsla(${block.id * 20}, 70%, 50%, 0.5)`,
               top: block.cord?.top ?? 'auto',
               left: block.cord?.left ?? 'auto',
               bottom: block.cord?.bottom ?? 'auto',
@@ -171,7 +203,7 @@ const SelectionOfApartments = () => {
             }}
             onClick={() => setCurrentApartment(block.id)}
           >
-             {block.id}
+        
           </div>
        
       )}
@@ -183,6 +215,61 @@ const SelectionOfApartments = () => {
           className="select-none w-full h-full min-h-[1080px] min-w-[1820px] object-cover absolute z-40 fade-in-out"
         />
       )}
+      <ConfigProvider
+            theme={{
+               components: {
+                  Drawer: {
+                    "colorBgElevated": "rgba(255,255,255,0)",
+                     footerPaddingBlock: 0,
+                     footerPaddingInline: 0,
+                     padding: 0,
+                     marginXS: 0,
+                     paddingLG: 0,
+                     paddingXS: 0,
+                  },
+               },
+            }}
+         >
+          
+      <Drawer
+     
+        placement="right"
+        closable={false}
+        onClose={() => setOpenDrawer(false)}
+        open={openDrawer}
+        width={300}
+        className='bg-black'
+       
+        
+        
+        >
+          <div className='glass p-4'  >
+            <div className='flex gap-4 justify-between items-center'>
+
+          <Button className='text-xl w-full' type='primary' size='large'> Random</Button>
+          <Button
+          type="primary"
+          shape="circle"
+          size="large"
+           className=" select-none  z-[55]"
+          icon={light ? <FaRegLightbulb /> : <FaLightbulb  className='text-yellow-500'/>}
+          onClick={() => setLight(!light)}
+        />
+            </div>
+<div className='grid grid-cols-2 overflow-y-auto p-4  gap-[10px] max-h-[93vh] '>
+       {blockCoordinates.map((item)=>(
+         <div onClick={()=>{
+           setCurrentApartment(item.id);
+           setOpenDrawer(!openDrawer)
+          }}  className={`flex items-center justify-center text-white w-full h-[80px] text-center text-3xl  ${item.id===currentApartment?"bg-green-500":"bg-blue-500"} shshsh rounded-2xl` }>{item.id}</div>
+        ))}
+        </div>
+        </div>
+      </Drawer>
+      </ConfigProvider>
+     
+
+
     </div>
   );
 };
